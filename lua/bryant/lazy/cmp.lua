963ac99317fae12be2e5ddf69e8af6028e1637d7
Bryant Paz <pazbryant7@outlook.com>
2024-07-19 02:14:52 -0600

    chore: update cmp configuration

--- lua/bryant/lazy/cmp.lua
+++ lua/bryant/lazy/cmp.lua
@@ -72,14 +72,6 @@ return {
 						luasnip.jump(-1)
 					end
 				end, { 'i', 's' }),
-
-				['<C-j>'] = cmp.mapping(function()
-					cmp.mapping.scroll_docs(-4)
-				end, { 'i', 's' }),
-
-				['<C-k>'] = cmp.mapping(function()
-					cmp.mapping.scroll_docs(4)
-				end, { 'i', 's' }),
 			}),
 			sources = {
 				{ name = 'nvim_lsp' },
