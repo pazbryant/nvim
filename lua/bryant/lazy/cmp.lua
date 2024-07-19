929671627f19ce4e85b58700920dd924472dcc8a
Bryant Paz <pazbryant7@outlook.com>
2024-07-19 02:11:58 -0600

    chore: update nvim config

--- lua/bryant/lazy/cmp.lua
+++ lua/bryant/lazy/cmp.lua
@@ -12,6 +12,18 @@ return {
 				require('luasnip.loaders.from_vscode').lazy_load({})
 			end,
 		},
+		{
+			'windwp/nvim-autopairs',
+			event = 'InsertEnter',
+			config = function()
+				require('nvim-autopairs').setup({
+					fast_wrap = {},
+					disable_filetype = { 'TelescopePrompt', 'vim' },
+				})
+				local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+				require('cmp').event:on('confirm_done', cmp_autopairs.on_confirm_done())
+			end,
+		},
 		-- sources
 		'hrsh7th/cmp-buffer',
 		'hrsh7th/cmp-path',
@@ -20,8 +32,6 @@ return {
 	config = function()
 		local cmp = require('cmp')
 		local luasnip = require('luasnip')
-		local cmp_types = require('cmp.types')
-
 		cmp.setup({
 			snippet = {
 				expand = function(args)
@@ -31,6 +41,7 @@ return {
 			completion = {
 				completeopt = 'menu,menuone,noinsert',
 			},
+			preselect = cmp.PreselectMode.None,
 			mapping = cmp.mapping.preset.insert({
 				['<C-n>'] = cmp.mapping.select_next_item(),
 				['<C-p>'] = cmp.mapping.select_prev_item(),
@@ -46,38 +57,20 @@ return {
 						luasnip.jump(-1)
 					end
 				end, { 'i', 's' }),
+				['<C-j>'] = cmp.mapping(function()
+					cmp.mapping.scroll_docs(-4)
+				end, { 'i', 's' }),
+				['<C-k>'] = cmp.mapping(function()
+					cmp.mapping.scroll_docs(4)
+				end, { 'i', 's' }),
 			}),
 			sources = {
+				{ name = 'nvim_lsp' },
 				{ name = 'luasnip' },
 				{ name = 'buffer' },
 				{ name = 'path' },
+				{ name = 'buffer' },
 			},
 		})
-
-		-- Toggle cmp custom function
-
-		local usercmd = vim.api.nvim_create_user_command
-
-		local enabled = true
-		local function toggle_cmp()
-			enabled = not enabled
-			if enabled then
-				cmp.setup({
-					completion = {
-						autocomplete = {
-							cmp_types.cmp.TriggerEvent.TextChanged,
-						},
-					},
-				})
-			else
-				cmp.setup({
-					completion = {
-						autocomplete = false,
-					},
-				})
-			end
-		end
-
-		usercmd('CmpToggle', toggle_cmp, { nargs = 0 })
 	end,
 }
---
+++ lua/bryant/lazy/leap.lua
@@ -0,0 +1,7 @@
+return {
+	'ggandor/leap.nvim',
+	lazy = false,
+	init = function()
+		require('leap').create_default_mappings()
+	end,
+}
