4a571e22691a6823666ac59dad0da81431330821
Bryant Paz <pazbryant7@outlook.com>
2024-07-21 06:15:33 -0600

    chore: user cmds themes

--- lua/bryant/usercmd.lua
+++ lua/bryant/usercmd.lua
@@ -1,18 +1,34 @@
 local usercmd = vim.api.nvim_create_user_command
 
-usercmd('SpellToggle', function()
+usercmd('Spelltoggle', function()
 	vim.cmd([[set spell!]])
 end, { nargs = 0 })
 
-usercmd('RelativeNumberToggle', function()
+usercmd('Relativenumbertoggle', function()
 	vim.cmd([[set rnu!]])
 end, { nargs = 0 })
 
-usercmd('DiagnosticsToggle', function()
+usercmd('Diagnostictoggle', function()
 	local current_value = vim.diagnostic.is_disabled()
 	if current_value then
 		vim.diagnostic.enable()
 	else
 		vim.diagnostic.disable()
 	end
+end, { nargs = 0 })
+
+usercmd('Frappe', function()
+	vim.cmd([[colorscheme catppuccin-frappe]])
+end, { nargs = 0 })
+
+usercmd('Rosepine', function()
+	vim.cmd([[colorscheme rose-pine]])
+end, { nargs = 0 })
+
+usercmd('Solarized', function()
+	vim.cmd([[colorscheme solarized]])
+end, { nargs = 0 })
+
+usercmd('Osaka', function()
+	vim.cmd([[colorscheme solarized-osaka]])
 end, { nargs = 0 })
