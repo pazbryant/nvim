7bb2ac35759336acf64b4f507a44690793a3beb7
Bryant Paz <pazbryant@proton.me>
2024-08-07 11:46:31 -0600

    chore: remove unused keybinds

--- lua/bryant/remap.lua
+++ lua/bryant/remap.lua
@@ -10,7 +10,6 @@ map('n', 'J', 'mzJ`z', { desc = 'One row align items' })
 -- center scren when navigate with nvim default key binds
 map('n', '<C-d>', '<C-d>zz', { desc = 'Better c-d command' })
 map('n', '<C-u>', '<C-u>zz', { desc = 'Better c-u command' })
-map('n', 'n', 'nzzzv', { desc = 'Better "n" command' })
 map('n', '{', '{zz', { desc = 'Better go to the next block command' })
 map('n', '}', '}zz', { desc = 'Better go to the previous block command' })
 map('n', '<C-o>', '<C-o>zz', { desc = 'Better go to the previous location' })
@@ -37,20 +36,19 @@ map(
 map('v', '<', '<gv', { desc = 'Better align items' })
 map('v', '>', '>gv', { desc = 'Better align items' })
 
--- move elements in visual mode
-map('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move elements up' })
-map('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move elements down' })
-
-map({ 'n', 'v' }, 'H', '^', { desc = 'Move the start of the line' })
-map({ 'n', 'v' }, 'L', 'g_', { desc = 'Move at the end of the line ' })
-
 -- use Telescope for spell suggest
 map('n', 'z=', '<cmd>Telescope spell_suggest<CR>', { desc = 'Suggest spell' })
+
+-- move between nvim split buffers
+map({ 'n', 'v', 'x' }, '<M-h>', '<C-w>h', { desc = 'Go to left window' })
+map({ 'n', 'v', 'x' }, '<M-l>', '<C-w>l', { desc = 'Go to right window' })
+map({ 'n', 'v', 'x' }, '<M-j>', '<C-w>j', { desc = 'Go to down window' })
+map({ 'n', 'v', 'x' }, '<M-k>', '<C-w>k', { desc = 'Go to up window' })
 
 -- user cmds
 map(
 	'n',
-	'<leader>dt',
+	'<leader>vtt',
 	'<cmd>Virtulatexttoggle<CR>',
 	{ desc = 'Virtulatex toggle' }
 )
