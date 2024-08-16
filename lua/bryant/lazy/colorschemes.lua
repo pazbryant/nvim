12b38f88d300e460c15cec5be6886895bfe64632
Bryant Paz <pazbryant@proton.me>
2024-08-16 17:51:28 -0600

    feat: update nvim plugins

--- lua/bryant/lazy/colorschemes.lua
+++ lua/bryant/lazy/colorschemes.lua
@@ -1,12 +1,37 @@
 local themes = {
 	{
-		'projekt0n/github-nvim-theme',
+		'Mofiqul/vscode.nvim',
 		enabled = true,
 		lazy = false,
 		priority = 1000,
+		opts = {
+			transparent = false,
+			italic_comments = true,
+			underline_links = true,
+			disable_nvimtree_bg = true,
+		},
 		config = function(_, opts)
-			vim.cmd([[colorscheme github_light_colorblind]])
-			require('github-theme').setup(opts)
+			vim.o.background = 'dark'
+			require('vscode').setup(opts)
+		end,
+	},
+
+	{
+		'rose-pine/neovim',
+		enabled = true,
+		name = 'rose-pine',
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require('rose-pine').setup({
+				variant = 'main', -- main, moon, or dawn
+				dark_variant = 'main', -- main, moon
+				styles = {
+					italic = false,
+					transparency = false,
+				},
+			})
+			vim.cmd([[colorscheme rose-pine]])
 		end,
 	},
 }
