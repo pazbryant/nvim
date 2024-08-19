2373ec724b527abc922e5d69b0294cc4b1a1ad33
Bryant Paz <pazbryant@proton.me>
2024-08-19 13:48:51 -0600

    feat: update tmux navigator config

--- lua/bryant/lazy/tmux-navigator.lua
+++ lua/bryant/lazy/tmux-navigator.lua
@@ -1,6 +1,8 @@
 return {
 	'christoomey/vim-tmux-navigator',
-  lazy = false,
+	init = function()
+		vim.g.tmux_navigator_no_mappings = 1
+	end,
 	cmd = {
 		'TmuxNavigateLeft',
 		'TmuxNavigateDown',
@@ -9,10 +11,10 @@ return {
 		'TmuxNavigatePrevious',
 	},
 	keys = {
-		{ '<M-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
-		{ '<M-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
-		{ '<M-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
-		{ '<M-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
-		{ '<M-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+		{ '<M-h>', '<cmd>TmuxNavigateLeft<cr>' },
+		{ '<M-j>', '<cmd>TmuxNavigateDown<cr>' },
+		{ '<M-k>', '<cmd>TmuxNavigateUp<cr>' },
+		{ '<M-l>', '<cmd>TmuxNavigateRight<cr>' },
+		{ '<M-\\>', '<cmd>TmuxNavigatePrevious<cr>' },
 	},
 }
