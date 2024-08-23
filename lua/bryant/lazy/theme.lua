592424aca36d399da00caf6234de391a3f174b8e
Bryant Paz <pazbryant@proton.me>
2024-08-22 19:07:49 -0600

    chore: update color scheme

--- lua/bryant/lazy/theme.lua
+++ lua/bryant/lazy/theme.lua
@@ -1,42 +1,5 @@
 local themes = {
 	{
-		'Mofiqul/vscode.nvim',
-		enabled = true,
-		lazy = false,
-		priority = 1000,
-		opts = {
-			transparent = false,
-			italic_comments = true,
-			underline_links = true,
-			disable_nvimtree_bg = true,
-		},
-		config = function(_, opts)
-			require('vscode').setup(opts)
-			vim.o.background = 'dark'
-			vim.cmd([[colorscheme vscode]])
-		end,
-	},
-
-	{
-		'rose-pine/neovim',
-		enabled = true,
-		name = 'rose-pine',
-		lazy = false,
-		priority = 1000,
-		opts = {
-			variant = 'main', -- main, moon, or dawn
-			dark_variant = 'main', -- main, moon
-			styles = {
-				italic = false,
-				transparency = false,
-			},
-		},
-		config = function(_, opts)
-			require('rose-pine').setup(opts)
-		end,
-	},
-
-	{
 		'projekt0n/github-nvim-theme',
 		enabled = true,
 		lazy = false,
@@ -48,6 +11,7 @@ local themes = {
 		},
 		config = function(_, opts)
 			require('github-theme').setup(opts)
+			vim.cmd([[colorscheme github_light]])
 		end,
 	},
 }
