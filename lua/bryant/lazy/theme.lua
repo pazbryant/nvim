a6e23b6555530a30dbee9b74efc3c76619c78b4c
Bryant Paz <pazbryant@proton.me>
2024-09-06 16:02:09 -0600

    chore: update color scheme

--- lua/bryant/lazy/theme.lua
+++ lua/bryant/lazy/theme.lua
@@ -11,7 +11,7 @@ local themes = {
 		},
 		config = function(_, opts)
 			require('github-theme').setup(opts)
-			vim.cmd([[colorscheme github_light]])
+			vim.cmd([[colorscheme github_dark]])
 		end,
 	},
 }
