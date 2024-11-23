a11e48b6a8ddc2c6e7ff1d6a87ac74d81d148170
Bryant Paz <pazbryant@proton.me>
2024-11-22 18:29:41 -0600

    chore: update config

--- lua/bryant/set.lua
+++ lua/bryant/set.lua
@@ -45,7 +45,7 @@ opt.spell = false
 opt.spelllang = { 'en_us', 'es_mx' }
 
 -- set spell file
-opt.spellfile = '/home/bryant/spell/en.utf-8.add'
+opt.spellfile = '/home/bryant/.config/spell/en.utf-8.add'
 
 -- disable nvim startup screen
 opt.shortmess:append('sI')
