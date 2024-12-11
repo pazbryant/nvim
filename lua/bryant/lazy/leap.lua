aac196afff8d8f96d3ad2b80dc1d801dfe4d99b2
Bryant Paz <pazbryant@proton.me>
2024-12-10 23:49:34 -0600

    chore: adding leap to core plugins

---
+++ lua/bryant/lazy/leap.lua
@@ -0,0 +1,17 @@
+return {
+	'ggandor/leap.nvim',
+	dependencies = 'tpope/vim-repeat',
+	lazy = false,
+	init = function()
+		require('leap').create_default_mappings()
+
+		-- Define equivalence classes for brackets and quotes, in addition to
+		-- the default whitespace group.
+		require('leap').opts.equivalence_classes =
+			{ ' \t\r\n', '([{', ')]}', '\'"`' }
+
+		-- Use the traversal keys to repeat the previous motion without explicitly
+		-- invoking Leap.
+		require('leap.user').set_repeat_keys('<enter>', '<backspace>')
+	end,
+}
--- lua/bryant/usercmd.lua
+++ lua/bryant/usercmd.lua
@@ -38,3 +38,21 @@ end, {
 	nargs = 0,
 	desc = 'Toggles virtual text display for diagnostics.',
 })
+
+-- fix leap cursor issues hidding the (real) cursor when leaping,
+-- and restore it afterwards.
+usercmd('LeapEnter', function()
+	vim.cmd.hi('Cursor', 'blend=100')
+	vim.opt.guicursor:append({ 'a:Cursor/lCursor' })
+end, {
+	nargs = 0,
+	desc = 'Append custom cursor style',
+})
+
+usercmd('LeapLeave', function()
+	vim.cmd.hi('Cursor', 'blend=0')
+	vim.opt.guicursor:remove({ 'a:Cursor/lCursor' })
+end, {
+	nargs = 0,
+	desc = 'Toggles virtual text display for diagnostics.',
+})
