feb2b54849963a55e142f321a890678d45b8bfb3
Bryant Paz <pazbryant@proton.me>
2024-12-16 05:30:11 -0600

    chore: adding nvim-lint

--- lua/bryant/plugins/lsp/init.lua
+++ lua/bryant/plugins/lsp/init.lua
@@ -158,4 +158,40 @@ return {
 			require('conform').setup(opts)
 		end,
 	},
+	{
+		'mfussenegger/nvim-lint',
+		event = { 'BufReadPre', 'BufNewFile' },
+		config = function()
+			local lint = require('lint')
+			lint.linters_by_ft = {
+				sh = { 'shellcheck' },
+				bash = { 'shellcheck' },
+				zsh = { 'shellcheck' },
+				lua = { 'luacheck' },
+				c = { 'clangtidy' },
+				go = { 'golangcilint' },
+				python = { 'ruff' },
+				markdown = { 'alex', 'markdownlint' },
+				javascript = { 'eslint_d' },
+				typescript = { 'eslint_d' },
+				typescriptreact = { 'eslint_d' },
+				javascriptreact = { 'eslint_d' },
+			}
+
+			-- needs to be removed to be used with older eslint configuartions < 9
+			local eslint_d = require('lint.linters.eslint_d')
+			eslint_d.args = vim.tbl_extend('force', {
+				'--config',
+				function()
+					return vim.fn.getcwd() .. '/eslint.config.js'
+				end,
+			}, eslint_d.args)
+
+			vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+				callback = function()
+					require('lint').try_lint()
+				end,
+			})
+		end,
+	},
 }
