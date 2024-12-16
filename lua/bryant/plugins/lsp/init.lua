7c16c687984ad9d68f164e4ea3e45e25d9d9f65f
Bryant Paz <pazbryant@proton.me>
2024-12-16 04:55:43 -0600

    chore: remove nvim-lint

--- lua/bryant/plugins/lsp/init.lua
+++ lua/bryant/plugins/lsp/init.lua
@@ -158,40 +158,4 @@ return {
 			require('conform').setup(opts)
 		end,
 	},
-	{
-		'mfussenegger/nvim-lint',
-		event = {
-			'BufReadPre',
-			'BufNewFile',
-		},
-		opts = {
-			linters_by_ft = {
-				sh = { 'shellcheck' },
-				bash = { 'shellcheck' },
-				zsh = { 'shellcheck' },
-				lua = { 'luacheck' },
-				c = { 'clangtidy' },
-				go = { 'golangcilint' },
-				python = { 'ruff' },
-				markdown = { 'alex', 'markdownlint' },
-				javascript = { 'eslint_d' },
-				typescript = { 'eslint_d' },
-				typescriptreact = { 'eslint_d' },
-				javascriptreact = { 'eslint_d' },
-			},
-		},
-		config = function()
-			local lint = require('lint')
-			local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-			vim.api.nvim_create_autocmd(
-				{ 'BufEnter', 'BufWritePost', 'InsertLeave' },
-				{
-					group = lint_augroup,
-					callback = function()
-						lint.try_lint()
-					end,
-				}
-			)
-		end,
-	},
 }
