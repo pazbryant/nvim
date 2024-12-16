52689b1d2380bd7d21f32746e975870caffb43cb
Bryant Paz <pazbryant@proton.me>
2024-12-16 03:38:29 -0600

    chore: adding a new source to cpm and new keybinds

--- lua/bryant/plugins/cmp.lua
+++ lua/bryant/plugins/cmp.lua
@@ -71,8 +71,15 @@ return {
 						luasnip.jump(-1)
 					end
 				end, { 'i', 's' }),
+				['<M-b>'] = cmp.mapping.scroll_docs(-4),
+				['<M-f>'] = cmp.mapping.scroll_docs(4),
 			}),
 			sources = {
+				{
+					name = 'lazydev',
+					-- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
+					group_index = 0,
+				},
 				{ name = 'nvim_lsp' },
 				{ name = 'luasnip' },
 				{ name = 'buffer' },
