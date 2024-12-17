dd6c0860dd23358b262353597f58783267179dfa
Bryant Paz <pazbryant@proton.me>
2024-12-17 03:04:38 -0600

    chore(neoscroll): new plugin

---
+++ lua/bryant/plugins/neoscroll.lua
@@ -0,0 +1,17 @@
+return {
+	'karb94/neoscroll.nvim',
+	lazy = false,
+	opts = {
+		mappings = { -- Keys to be mapped to their corresponding default scrolling animation
+			'<C-u>',
+			'<C-d>',
+			'<C-b>',
+			'<C-f>',
+			'<C-y>',
+			'<C-e>',
+			'zt',
+			'zz',
+			'zb',
+		},
+	},
+}
