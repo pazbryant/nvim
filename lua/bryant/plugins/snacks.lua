09a729d03ea3e73686d8ed4321eaf0a491d796a7
Bryant Paz <pazbryant@proton.me>
2024-12-18 01:27:18 -0600

    chore(snacks): New collections of plugins added. Check commit description.
    - Adding animate plugin, it creates basic motion animations
    - Adding inentline replacinb blankline previous plugin
    - Replacing zen plugin
    
    For more information is needed to review the file and the changes.

--- lua/bryant/plugins/snacks.lua
+++ lua/bryant/plugins/snacks.lua
@@ -8,10 +8,57 @@ return {
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
 		bigfile = { enabled = true },
+		statuscolumn = { enabled = true },
+		lazygit = { enable = true },
 		indent = { enabled = true },
 		quickfile = { enabled = true },
 		scope = { enabled = true },
+		terminal = { enabled = false },
+		toggle = { enabled = false },
+		notifier = { enabled = true, timeout = 3000 },
 		scroll = { enabled = true },
 		zen = { enabled = true },
+		styles = {
+			zen = {
+				backdrop = { transparent = true, blend = 20 },
+			},
+		},
+	},
+	keys = {
+		{
+			'zm',
+			function()
+				Snacks.zen()
+			end,
+			desc = 'Toggle Zen Mode',
+		},
+		{
+			'<leader>dn',
+			function()
+				Snacks.notifier.hide()
+			end,
+			desc = 'Dismiss All Notifications',
+		},
+		{
+			'<leader>n',
+			function()
+				Snacks.notifier.show_history()
+			end,
+			desc = 'Notification History',
+		},
+		{
+			'<leader>rf',
+			function()
+				Snacks.rename.rename_file()
+			end,
+			desc = 'Rename File',
+		},
+		{
+			'<leader>gg',
+			function()
+				Snacks.lazygit()
+			end,
+			desc = 'Lazygit',
+		},
 	},
 }
