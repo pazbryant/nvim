3f281e14b29a706bf0c8cd83bfe074f3d1e47fb5
Bryant Paz <pazbryant@proton.me>
2024-12-18 04:27:57 -0600

    chore: update snacks config

--- lua/bryant/plugins/snacks.lua
+++ lua/bryant/plugins/snacks.lua
@@ -7,16 +7,29 @@ return {
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
+		animate = { enabled = false },
+		scratch = { enabled = false },
+		profiler = { enabled = false },
+		debug = { enabled = false },
+		dim = { enabled = false },
+		git = { enabled = false },
+		gitbrowse = { enabled = false },
+		bufdelete = { enabled = false },
+		terminal = { enabled = false },
+		toggle = { enabled = false },
+		util = { enabled = false },
+		win = { enabled = false },
+		scroll = { enabled = false },
+		statuscolumn = { enabled = false },
+		-- Enabled
+		rename = { enabled = true },
 		bigfile = { enabled = true },
-		statuscolumn = { enabled = true },
 		lazygit = { enable = true },
 		indent = { enabled = true },
 		quickfile = { enabled = true },
 		scope = { enabled = true },
-		terminal = { enabled = false },
-		toggle = { enabled = false },
 		notifier = { enabled = true, timeout = 3000 },
-		scroll = { enabled = true },
+		notify = { enabled = true },
 		zen = { enabled = true },
 		styles = {
 			zen = {
