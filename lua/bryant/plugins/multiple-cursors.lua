cfe20e4df32bf2d0e04a00d7cf080927511b4029
Bryant Paz <pazbryant@proton.me>
2024-12-21 22:49:30 -0600

    chore(multiple cursors): update config

--- lua/bryant/plugins/multiple-cursors.lua
+++ lua/bryant/plugins/multiple-cursors.lua
@@ -13,18 +13,39 @@ return {
 		map({ 'n', 'v' }, '<C-n>', function()
 			mc.matchAddCursor(1)
 		end)
-		map({ 'n', 'v' }, '<M-s>', function()
+		map({ 'n', 'v' }, '<C-S>n', function()
 			mc.matchSkipCursor(1)
 		end)
-		map({ 'n', 'v' }, '<leader>n', function()
-			mc.matchAddCursor(-1)
+
+		-- Add or skip cursor above/below the main cursor.
+		map({ 'n', 'v' }, '<up>', function()
+			mc.lineAddCursor(-1)
 		end)
-		map({ 'n', 'v' }, '<leader>s', function()
-			mc.matchSkipCursor(-1)
+		map({ 'n', 'v' }, '<down>', function()
+			mc.lineAddCursor(1)
 		end)
+		map({ 'n', 'v' }, '<leader><up>', function()
+			mc.lineSkipCursor(-1)
+		end)
+		map({ 'n', 'v' }, '<leader><down>', function()
+			mc.lineSkipCursor(1)
+		end)
+
+		-- Add all matches in the document
+		map({ 'n', 'v' }, '<leader>A', mc.matchAllAddCursors)
+
+		-- Rotate the main cursor.
+		map({ 'n', 'v' }, '<left>', mc.nextCursor)
+		map({ 'n', 'v' }, '<right>', mc.prevCursor)
+
+		-- Delete the main cursor.
+		map({ 'n', 'v' }, '<C-x>', mc.deleteCursor)
 
 		-- Add and remove cursors with control + left click.
 		map('n', '<c-leftmouse>', mc.handleMouse)
+
+		-- Easy way to add and remove cursors using the main cursor.
+		map({ 'n', 'v' }, '<c-q>', mc.toggleCursor)
 
 		map('n', '<esc>', function()
 			if not mc.cursorsEnabled() then
@@ -39,6 +60,14 @@ return {
 		-- Append/insert for each line of visual selections.
 		map('v', 'I', mc.insertVisual)
 		map('v', 'A', mc.appendVisual)
+
+		-- Rotate visual selection contents.
+		map('v', '<leader>t', function()
+			mc.transposeCursors(1)
+		end)
+		map('v', '<leader>T', function()
+			mc.transposeCursors(-1)
+		end)
 
 		-- Customize how cursors look.
 		local hl = vim.api.nvim_set_hl
