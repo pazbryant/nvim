b31126bcbf51e9e951394e7280f55a2f1f426263
Bryant Paz <pazbryant@proton.me>
2024-12-25 03:36:51 -0600

    chore(trouble): Update config

--- lua/bryant/plugins/trouble.lua
+++ lua/bryant/plugins/trouble.lua
@@ -4,18 +4,26 @@ return {
 	opts = {
 		auto_close = true,
 		focus = true,
+		---@type trouble.Window.opts
+		win = {
+			size = {
+				height = 20,
+			},
+		}, -- window options for the results window. Can be a split or a floating window.
+		-- Window options for the preview window. Can be a split, floating window,
+		-- or `main` to show the preview in the main editor window.
 	},
 	cmd = 'Trouble',
 	keys = {
 		{
 			'<leader>x',
-			'<cmd>Trouble diagnostics toggle<cr>',
-			desc = 'Diagnostics (Trouble)',
+			'<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+			desc = 'Buffer Diagnostics (Trouble)',
 		},
 		{
 			'<leader>X',
-			'<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
-			desc = 'Buffer Diagnostics (Trouble)',
+			'<cmd>Trouble diagnostics toggle<cr>',
+			desc = 'Diagnostics (Trouble)',
 		},
 		{
 			'<leader>cs',
@@ -31,11 +39,6 @@ return {
 			'<leader>l',
 			'<cmd>Trouble loclist toggle<cr>',
 			desc = 'Location List (Trouble)',
-		},
-		{
-			'<leader>q',
-			'<cmd>Trouble qflist toggle<cr>',
-			desc = 'Quickfix List (Trouble)',
 		},
 	},
 }
