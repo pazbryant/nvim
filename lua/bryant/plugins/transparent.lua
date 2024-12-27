20fc5a9362225025f608a75bf823d1f217534188
Bryant Paz <pazbryant@proton.me>
2024-12-26 22:41:28 -0600

    chore(nvim-transparent): new plugin

---
+++ lua/bryant/plugins/transparent.lua
@@ -0,0 +1,14 @@
+-- Optional, you don't have to run setup.
+return {
+	'xiyaowong/transparent.nvim',
+	cmd = { 'TransparentEnable', 'TransparentDisable', 'TransparentToggle' },
+	opts = {},
+	keys = {
+		{
+			'<leader>tg',
+			"<cmd>TransparentToggle<CR>",
+			desc = "Toggle transparency"
+		}
+	}
+
+}
