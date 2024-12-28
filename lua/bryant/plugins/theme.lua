b0b9011e58f6e66eb65a22f53097373b2058a4ea
Bryant Paz <pazbryant@proton.me>
2024-12-27 23:35:06 -0600

    chore: update knagawa config

--- lua/bryant/plugins/theme.lua
+++ lua/bryant/plugins/theme.lua
@@ -31,19 +31,20 @@ local themes = {
 
 	{
 		'rebelot/kanagawa.nvim',
-		config = function()
-			require('kanagawa').setup({
-				compile = true,
-				commentStyle = { italic = true },
-				keywordStyle = { italic = true },
-				statementStyle = { bold = true },
-				transparent = true,
-				theme = 'wave', -- Load "wave" theme when 'background' option is not set
-				background = { -- map the value of 'background' option to a theme
-					dark = 'wave', -- try "dragon" !
-					light = 'lotus',
-				},
-			})
+		opts = {
+			compile = true,
+			commentStyle = { italic = true },
+			keywordStyle = { italic = true },
+			statementStyle = { bold = true },
+			transparent = true,
+			theme = 'wave', -- Load "wave" theme when 'background' option is not set
+			background = { -- map the value of 'background' option to a theme
+				dark = 'wave', -- try "dragon" !
+				light = 'lotus',
+			},
+		},
+		config = function(_, opts)
+			require('kanagawa').setup(opts)
 			vim.cmd.colorscheme('kanagawa')
 		end,
 	},
