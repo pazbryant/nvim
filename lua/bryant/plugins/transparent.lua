fdf8c52e905fe2dc9d492e44dbb26497cf204f1f
Bryant Paz <pazbryant@proton.me>
2024-12-27 23:22:28 -0600

    chore: Enable transparency by default

--- lua/bryant/plugins/transparent.lua
+++ lua/bryant/plugins/transparent.lua
@@ -1,14 +1,20 @@
 -- Optional, you don't have to run setup.
 return {
 	'xiyaowong/transparent.nvim',
-	cmd = { 'TransparentEnable', 'TransparentDisable', 'TransparentToggle' },
+	init = function()
+		vim.cmd([[TransparentEnable]])
+	end,
+	cmd = {
+		'TransparentEnable',
+		'TransparentDisable',
+		'TransparentToggle',
+	},
 	opts = {},
 	keys = {
 		{
 			'<leader>tg',
-			"<cmd>TransparentToggle<CR>",
-			desc = "Toggle transparency"
-		}
-	}
-
+			'<cmd>TransparentToggle<CR>',
+			desc = 'Toggle transparency',
+		},
+	},
 }
