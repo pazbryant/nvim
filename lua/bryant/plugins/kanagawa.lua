7272e70501aca378121cc07717175a611f156f81
Bryant Paz <pazbryant@proton.me>
2024-12-28 23:48:42 -0600

    chore: remove unused color schemes

---
+++ lua/bryant/plugins/kanagawa.lua
@@ -0,0 +1,21 @@
+return {
+	'rebelot/kanagawa.nvim',
+	lazy = false,
+	priority = 1000,
+	opts = {
+		compile = true,
+		commentStyle = { italic = true },
+		keywordStyle = { italic = true },
+		statementStyle = { bold = true },
+		transparent = true,
+		theme = 'wave', -- Load "wave" theme when 'background' option is not set
+		background = { -- map the value of 'background' option to a theme
+			dark = 'wave', -- try "dragon" !
+			light = 'lotus',
+		},
+	},
+	config = function(_, opts)
+		require('kanagawa').setup(opts)
+		vim.cmd('colorscheme kanagawa')
+	end,
+}
