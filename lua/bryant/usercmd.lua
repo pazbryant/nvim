15f01962a23ad839305804a21eab6283dae5106a
Bryant Paz <pazbryant@proton.me>
2024-12-28 23:58:33 -0600

    feat: Nes plugins without configuration
    - Still left config neotest plugin
    - Still lelt config dap plugin

--- lua/bryant/usercmd.lua
+++ lua/bryant/usercmd.lua
@@ -1,23 +1,5 @@
 local usercmd = vim.api.nvim_create_user_command
 
--- fix leap cursor issues hidding the (real) cursor when leaping,
--- and restore it afterwards.
-usercmd('LeapEnter', function()
-	vim.cmd.hi('Cursor', 'blend=100')
-	vim.opt.guicursor:append({ 'a:Cursor/lCursor' })
-end, {
-	nargs = 0,
-	desc = 'Append custom cursor style',
-})
-
-usercmd('LeapLeave', function()
-	vim.cmd.hi('Cursor', 'blend=0')
-	vim.opt.guicursor:remove({ 'a:Cursor/lCursor' })
-end, {
-	nargs = 0,
-	desc = 'Toggles virtual text display for diagnostics.',
-})
-
 -- Function to sort lines by length
 usercmd('Sort', function()
 	local start_line = vim.fn.line("'<")
