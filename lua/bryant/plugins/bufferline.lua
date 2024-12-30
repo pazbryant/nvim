35ae2a7c13783a56c9be2b5a870531ffb83d8dc6
Bryant Paz <pazbryant@proton.me>
2024-12-29 20:58:06 -0600

    chore(bufferline): only show current buffer

--- lua/bryant/plugins/bufferline.lua
+++ lua/bryant/plugins/bufferline.lua
@@ -3,5 +3,15 @@ return {
 	version = '*',
 	lazy = false,
 	dependencies = 'nvim-tree/nvim-web-devicons',
-	opts = {},
+	config = function()
+		require('bufferline').setup({
+			options = {
+				custom_filter = function(buf_number)
+					-- Show only the current buffer
+					local current_buffer = vim.api.nvim_get_current_buf()
+					return buf_number == current_buffer
+				end,
+			},
+		})
+	end,
 }
