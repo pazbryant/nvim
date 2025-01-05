5f67ee0d3807b3d593f252a42909cfcddbb2385c
Bryant Paz <pazbryant@proton.me>
2025-01-05 03:52:55 -0600

    chore(aerial): new lazy bindings.

--- lua/bryant/plugins/aerial.lua
+++ lua/bryant/plugins/aerial.lua
@@ -9,13 +9,6 @@ return {
 		'nvim-treesitter/nvim-treesitter',
 		'nvim-tree/nvim-web-devicons',
 	},
-	keys = {
-		{
-			'<C-a>',
-			'<cmd>AerialToggle!<CR>',
-			desc = 'Toggle Aerial',
-		},
-	},
 	config = function()
 		require('aerial').setup({
 			layout = {
@@ -24,11 +17,23 @@ return {
 				min_width = 20,
 				default_direction = 'prefer_left',
 			},
-			on_attach = function(bufnr)
-				local map = vim.keymap.set
-				map('n', '[a', '<cmd>AerialPrev<CR>', { buffer = bufnr })
-				map('n', ']a', '<cmd>AerialNext<CR>', { buffer = bufnr })
-			end,
 		})
 	end,
+	keys = {
+		{
+			'<C-a>',
+			'<cmd>AerialToggle!<CR>',
+			desc = 'Toggle Aerial',
+		},
+		{
+			'[a',
+			'<cmd>AerialPrev<CR>',
+			desc = 'Aerial prev',
+		},
+		{
+			']a',
+			'<cmd>AerialNext<CR>',
+			desc = 'Aerial next',
+		},
+	},
 }
