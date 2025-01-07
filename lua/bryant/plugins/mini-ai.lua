c6b660d00e30fbc83fbf6821a17e326c29d65c91
Bryant Paz <pazbryant@proton.me>
2025-01-07 01:02:34 -0600

    chore(miniai): use only mini.ai instead of treesitter text objects

--- lua/bryant/plugins/mini-ai.lua
+++ lua/bryant/plugins/mini-ai.lua
@@ -1,46 +1,49 @@
 return {
 	'echasnovski/mini.ai',
-	lazy = false,
-	version = '*',
-	opts = {
-		custom_textobjects = nil,
-
-		-- Module mappings. Use `''` (empty string) to disable one.
-		mappings = {
-			-- Main textobject prefixes
-			around = 'a',
-			inside = 'i',
-
-			-- Next/last variants
-			around_next = 'an',
-			inside_next = 'in',
-			around_last = 'ae',
-			inside_last = 'ie',
-
-			-- Move cursor to corresponding edge of `a` textobject
-			goto_left = 'g[',
-			goto_right = 'g]',
-		},
-
-		-- Number of lines within which textobject is searched
-		n_lines = 50,
-
-		-- How to search for object (first inside current line, then inside
-		-- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
-		-- 'cover_or_nearest', 'next', 'previous', 'nearest'.
-		search_method = 'cover_or_next',
-
-		-- Whether to disable showing non-error feedback
-		-- This also affects (purely informational) helper messages shown after
-		-- idle time if user input is required.
-		silent = false,
+	keys = {
+		{ 'a', mode = { 'x', 'o' } },
+		{ 'i', mode = { 'x', 'o' } },
 	},
+	dependencies = 'nvim-treesitter-textobjects',
+	opts = function()
+		local ai = require('mini.ai')
+		return {
+			n_lines = 500,
+
+			mappings = {
+				-- Main textobject prefixes
+				around = 'a',
+				inside = 'i',
+
+				-- Next/last variants
+				around_next = 'an',
+				inside_next = 'in',
+				around_last = 'ae',
+				inside_last = 'ie',
+
+				-- Move cursor to corresponding edge of `a` textobject
+				goto_left = 'g[',
+				goto_right = 'g]',
+			},
+			custom_textobjects = {
+				-- Disable brackets alias in favor of builtin block textobject
+				b = false,
+
+				o = ai.gen_spec.treesitter({
+					a = { '@block.outer', '@conditional.outer', '@loop.outer' },
+					i = { '@block.inner', '@conditional.inner', '@loop.inner' },
+				}, {}),
+				F = ai.gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }, {}),
+				c = ai.gen_spec.treesitter({ a = '@class.outer', i = '@class.inner' }, {}),
+			},
+		}
+	end,
 }
 
 -- NOTE: Defaults keybinds
 
 -- q = '' "" ``
--- b = [] {} ()
+-- o = {[( if loop class )]}
 -- af = outer function
 -- if = inner function
 -- a = argument
@@ -49,3 +52,15 @@ return {
 -- *number = count from actual position
 -- g] = next text object
 -- g[ = previous text object
+
+
+--- NOTE: Example
+
+-- dio - delete inside block/conditional/loop
+-- dao - delete around block/conditional/loop
+-- yio - yank (copy) inside block/conditional/loop
+-- yao - yank (copy) around block/conditional/loop
+-- cio - change inside block/conditional/loop
+-- cao - change around block/conditional/loop
+-- vio - visually select inside block/conditional/loop
+-- vao - visually select around block/conditional/loop
