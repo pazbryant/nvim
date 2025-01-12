6b39dc0dee8cc49575364d4043f921b8f65acf2a
Bryant Paz <pazbryant@proton.me>
2025-01-11 23:26:02 -0600

    chore(autopair): Create a usercmd to enable it and disable it by default.

--- lua/bryant/plugins/auto-pairs.lua
+++ lua/bryant/plugins/auto-pairs.lua
@@ -1,11 +1,15 @@
 return {
 	'windwp/nvim-autopairs',
 	dependencies = 'hrsh7th/nvim-cmp',
-	event = 'InsertEnter',
 	init = function()
-		local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-		local cmp = require('cmp')
-		cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+		local usercmd = vim.api.nvim_create_user_command
+		usercmd('EnableAutoPair', function()
+			local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+			local cmp = require('cmp')
+			cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+		end, {
+			desc = 'Start autopairs plugin',
+		})
 	end,
-	config = true,
+	config = {},
 }
