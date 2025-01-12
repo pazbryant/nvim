7d7dc5ed46015127ade15187001d7cce680cc5c4
Bryant Paz <pazbryant@proton.me>
2025-01-12 05:36:55 -0600

    chore(cmp): use async path instead of defaulth path source.

--- lua/bryant/plugins/cmp.lua
+++ lua/bryant/plugins/cmp.lua
@@ -6,7 +6,7 @@ return {
 		'onsails/lspkind.nvim',
 		'hrsh7th/cmp-buffer',
 		'hrsh7th/cmp-nvim-lsp',
-		'hrsh7th/cmp-path',
+		'FelipeLema/cmp-async-path',
 		'hrsh7th/cmp-nvim-lsp-signature-help',
 		-- This is important for integrating LuaSnip with nvim-cmp
 		'saadparwaiz1/cmp_luasnip',
@@ -81,7 +81,7 @@ return {
 				},
 				{ name = 'luasnip' },
 				{ name = 'buffer' },
-				{ name = 'path' },
+				{ name = 'async_path' },
 				{ name = 'nvim_lsp_signature_help' },
 			},
 		})
