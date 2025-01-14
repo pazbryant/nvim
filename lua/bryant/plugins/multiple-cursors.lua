415d1a056baf6bb4e4b34bf9075ae761d915e118
Bryant Paz <pazbryant@proton.me>
2025-01-14 15:14:25 -0600

    chore(plugins): fix keybins to be compatible between plugins

--- lua/bryant/plugins/multiple-cursors.lua
+++ lua/bryant/plugins/multiple-cursors.lua
@@ -8,18 +8,26 @@ return {
 		local map = vim.keymap.set
 
 		-- stylua: ignore start
-		map({ 'n', 'v' }, '<right>', mc.prevCursor, { desc = 'Multicursor next' })
-		map('v', 'I', mc.insertVisual, { desc = 'Multicursor visual insert mode' })
-		map({ 'n', 'v' }, '<left>', mc.nextCursor, { desc = 'Multicursor previous' })
-		map('v', 'A', mc.appendVisual, { desc = 'Multicursor visual insert mode last' })
-		map({ 'n', 'v' }, '<C-x>', mc.deleteCursor, { desc = 'Multicursor remove cursor' })
-		map('n', '<c-leftmouse>', mc.handleMouse, { desc = 'Multicursor insert with mouse' })
 		map({ 'n', 'v' }, '<c-n>', function() mc.matchAddCursor(1) end, { desc = 'Multicursor start' })
 		map({ 'n', 'v' }, '<leader>A', mc.matchAllAddCursors, { desc = 'Multicursor add all occurrence' })
-		map({ 'n', 'v' }, '<leader>s', function() mc.matchSkipCursor(1) end, { desc = 'Multicursor skip' })
-		map({ 'n', 'v' }, '<leader>N', function() mc.matchAddCursor(-1) end, { desc = 'Multicursor minus one' })
-		map({ 'n', 'v' }, '<leader>B', function() mc.matchSkipCursor(-1) end, { desc = 'Multicursor match skip backwards' })
 		map('n', '<esc>', function() if not mc.cursorsEnabled() then mc.enableCursors() elseif mc.hasCursors() then mc.clearCursors() else vim.cmd('noh') end end, { desc = 'Multicursor clear cursors' })
+
+		map({ 'n', 'v' }, '<right>', mc.nextCursor, { desc = 'Multicursor next' })
+		map({ 'n', 'v' }, '<left>', mc.prevCursor, { desc = 'Multicursor previous' })
+
+		map({"n", "v"}, "<up>", function()     mc.lineAddCursor(-1) end)
+		map({ "n", "v" }, "down>", function() mc.lineAddCursor(1) end)
+
+		map({ "n", "v" }, "<leader><up>", function() mc.lineSkipCursor(-1) end)
+		map({ "n", "v" }, "<leader><down>", function() mc.lineSkipCursor(1) end)
+
+		map('v', 'q', mc.deleteCursor, { desc = 'Multicursor remove cursor' })
+		map('v', 'Q', function() mc.matchSkipCursor(1) end, { desc = 'Multicursor skip' })
+
+		map('v', 'A', mc.appendVisual, { desc = 'Multicursor visual insert mode last' })
+		map('v', 'I', mc.insertVisual, { desc = 'Multicursor visual insert mode' })
+
+		map('n', '<c-leftmouse>', mc.handleMouse, { desc = 'Multicursor insert with mouse' })
 
 		local hl = vim.api.nvim_set_hl
 		hl(0, 'MultiCursorCursor', { link = 'Cursor' })
