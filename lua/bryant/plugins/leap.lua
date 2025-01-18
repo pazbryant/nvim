12ddfb64e9a1e1ed131cd556d3f973fdedfde165
Bryant Paz <pazbryant@proton.me>
2025-01-18 15:38:56 -0600

    chore(plugins): leap, surround and visula surround work together

--- lua/bryant/plugins/leap.lua
+++ lua/bryant/plugins/leap.lua
@@ -1,6 +1,8 @@
 return {
 	'ggandor/leap.nvim',
+	lazy = false,
 	dependencies = {
+		'tpope/vim-repeat',
 		{
 			'ggandor/flit.nvim',
 			config = true,
@@ -11,15 +13,11 @@ return {
 				{ mode = { 'n', 'o', 'x' }, 'F' },
 			},
 		},
-		'tpope/vim-repeat',
 	},
-	lazy = false,
 	init = function()
 		local map = vim.keymap.set
-		map('n', 's', '<Plug>(leap)')
+		map('n', '<cr>', '<Plug>(leap)')
 		map('n', 'S', '<Plug>(leap-from-window)')
-		map({ 'x', 'o' }, 's', '<Plug>(leap-forward)')
-		map({ 'x', 'o' }, 'S', '<Plug>(leap-backward)')
 
 		-- Define equivalence classes for brackets and quotes, in addition to
 		-- the default whitespace group.
