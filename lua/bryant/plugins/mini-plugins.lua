ec02a1887db928b1e82fad414e34719dd0034222
Bryant Paz <pazbryant@proton.me>
2025-01-20 14:02:47 -0600

    chore(mini.operators): remove plugin

--- lua/bryant/plugins/mini-plugins.lua
+++ lua/bryant/plugins/mini-plugins.lua
@@ -88,19 +88,6 @@ return {
 	},
 
 	{
-		'echasnovski/mini.operators',
-		version = '*',
-		keys = {
-			{ 'g=', desc = 'Mini.operator Evaluate text' },
-			{ 'gx', desc = 'Mini.operator Exchange text' },
-			{ 'gm', desc = 'Mini.operator Multiply text' },
-			{ 'gr', desc = 'Mini.operator Replace text' },
-			{ 'gs', desc = 'Mini.operator Sort text' },
-		},
-		opts = {},
-	},
-
-	{
 		'echasnovski/mini.pairs',
 		event = 'InsertEnter',
 		version = '*',
