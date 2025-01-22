8515fb86193957cc84b6267ce90b07b1f6ba00d5
Bryant Paz <pazbryant@proton.me>
2025-01-21 22:53:09 -0600

    chore(obsidian): integrate witih fzf-lua and add attachmen configuration

--- lua/bryant/plugins/obsidian.lua
+++ lua/bryant/plugins/obsidian.lua
@@ -5,10 +5,7 @@ return {
 	cmd = {
 		'ObsidianToday',
 		'ObsidianTemplate',
-		'ObsidianNewFromTemplate',
 		'ObsidianNew',
-		'ObsidianFollowLink',
-		'ObsidianSearch',
 	},
 	opts = {
 		workspaces = {
@@ -45,6 +42,28 @@ return {
 				opts = { buffer = true, expr = true },
 			},
 		},
+
+		picker = {
+			-- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+			name = 'fzf-lua',
+			-- Optional, configure key mappings for the picker. These are the defaults.
+			-- Not all pickers support all mappings.
+			note_mappings = {
+				-- Create a new note from your query.
+				new = '<C-x>',
+				-- Insert a link to the selected note.
+				insert_link = '<C-l>',
+			},
+			tag_mappings = {
+				-- Add tag(s) to current note.
+				tag_note = '<C-x>',
+				-- Insert a tag at the current location.
+				insert_tag = '<C-l>',
+			},
+		},
+
+		preferred_link_style = 'markdown', -- wiki/markdown
+
 		follow_url_func = function(url)
 			-- vim.fn.jobstart({"xdg-open", url})  -- linux
 			vim.ui.open(url) -- need Neovim 0.10.0+
@@ -56,22 +75,29 @@ return {
 		follow_img_func = function(img)
 			vim.fn.jobstart({ 'xdg-open', img }) -- linux
 		end,
+
 		attachments = {
 			img_folder = 'obsidian/files',
+			img_name_func = function()
+				return string.format('%s-%s', os.time())
+			end,
+
+			-- A function that determines the text to insert in the note when pasting an image.
+			-- It takes two arguments, the `obsidian.Client` and an `obsidian.Path` to the image file.
+			-- This is the default implementation.
+			---@param client obsidian.Client
+			---@param path obsidian.Path the absolute path to the image file
+			---@return string
 			img_text_func = function(client, path)
 				path = client:vault_relative_path(path) or path
 				return string.format('![%s](%s)', path.name, path)
 			end,
 		},
-		image_name_func = function()
-			return string.format('%s-', os.time())
-		end,
 	},
 	-- stylua: ignore start
 	keys = {
 		{ '<leader>ot', '<cmd>ObsidianToday<CR>', desc = 'Open obsidian daily today' },
 		{ '<leader>oT', '<cmd>ObsidianTemplate<CR>', desc = 'Paste obsidian template current buffer' },
 		{ '<leader>on', '<cmd>ObsidianNew<CR>', desc = 'New obsidian note' },
-		{ '<leader>os', '<cmd>ObsidianSearch<CR>', desc = 'Search with obsidian' },
 	},
 }
