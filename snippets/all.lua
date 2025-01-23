df6c1293258ff9d1a705425be0c7ef3cab59e4e7
Bryant Paz <pazbryant@proton.me>
2025-01-23 01:11:19 -0600

    chore(snippets): update snippets for all type of files, new `code` snippet.

--- snippets/all.lua
+++ snippets/all.lua
@@ -8,44 +8,58 @@ return {
 	}),
 
 	--- FIX
-	s('fix', {
+	s('todo:fix', {
 		t('FIX: '),
 		i(1, 'describe what needs to be done'),
 	}),
 
 	-- HACK
-	s('hack', {
+	s('todo:hack', {
 		t('HACK: '),
 		i(1, 'explain the workaround or hack here'),
 	}),
 
 	-- WARN
-	s('warn', {
+	s('todo:warn', {
 		t('WARN: '),
 		i(1, 'explain the warning here'),
 	}),
 
 	-- PERF
-	s('perf', {
+	s('todo:perf', {
 		t('PERF: '),
 		i(1, 'describe the performance issue or suggestion'),
 	}),
 
 	-- NOTE
-	s('note', {
+	s('todo:note', {
 		t('NOTE: '),
 		i(1, 'add a note or explanation here'),
 	}),
 
 	-- TEST
-	s('test', {
+	s('todo:test', {
 		t('TEST: '),
 		i(1, 'describe the test case or issue here'),
 	}),
 
 	-- File type
-	s('ft', {
+	s('fyletype', {
 		t('vim: ft='),
 		i(1, 'type'),
+	}),
+
+	s('code', {
+		t('`'),
+		i(1),
+		t('`'),
+		i(2),
+	}),
+
+	s('code2', {
+		t('``'),
+		i(1),
+		t('``'),
+		i(2),
 	}),
 }
