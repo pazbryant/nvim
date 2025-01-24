fdcc653a87c5454eada265e745e0b98b583586eb
Bryant Paz <pazbryant@proton.me>
2025-01-24 01:26:59 -0600

    chore(markdown): new plugin to render better markdown files

---
+++ lua/bryant/plugins/markdown.lua
@@ -0,0 +1,12 @@
+return {
+	'MeanderingProgrammer/render-markdown.nvim',
+	ft = 'markdown',
+	opts = {},
+	keys = {
+		{
+			'<leader>mr',
+			'<cmd>RenderMarkdown toggle<cr>',
+			desc = 'Reder markdown',
+		},
+	},
+}
