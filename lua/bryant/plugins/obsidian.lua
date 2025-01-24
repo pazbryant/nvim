edae6d9b6488b8ccb19503ebc3df90147f7f1e1a
Bryant Paz <pazbryant@proton.me>
2025-01-24 01:26:47 -0600

    chore(obsidian): remove not used/working options

--- lua/bryant/plugins/obsidian.lua
+++ lua/bryant/plugins/obsidian.lua
@@ -5,7 +5,6 @@ return {
 	cmd = {
 		'ObsidianToday',
 		'ObsidianTemplate',
-		'ObsidianNew',
 	},
 	opts = {
 		workspaces = {
@@ -20,7 +19,7 @@ return {
 		},
 		completion = { nvim_cmp = true, min_chars = 2 },
 		disable_frontmatter = true,
-		ui = { enable = true },
+		ui = { enable = false },
 		templates = {
 			subdir = 'obsidian/templates',
 		},
@@ -42,62 +41,10 @@ return {
 				opts = { buffer = true, expr = true },
 			},
 		},
-
-		picker = {
-			-- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
-			name = 'fzf-lua',
-			-- Optional, configure key mappings for the picker. These are the defaults.
-			-- Not all pickers support all mappings.
-			note_mappings = {
-				-- Create a new note from your query.
-				new = '<C-x>',
-				-- Insert a link to the selected note.
-				insert_link = '<C-l>',
-			},
-			tag_mappings = {
-				-- Add tag(s) to current note.
-				tag_note = '<C-x>',
-				-- Insert a tag at the current location.
-				insert_tag = '<C-l>',
-			},
-		},
-
-		preferred_link_style = 'markdown', -- wiki/markdown
-
-		follow_url_func = function(url)
-			-- vim.fn.jobstart({"xdg-open", url})  -- linux
-			vim.ui.open(url) -- need Neovim 0.10.0+
-		end,
-
-		-- Optional, by default when you use `:ObsidianFollowLink` on a link to an image
-		-- file it will be ignored but you can customize this behavior here.
-		---@param img string
-		follow_img_func = function(img)
-			vim.fn.jobstart({ 'xdg-open', img }) -- linux
-		end,
-
-		attachments = {
-			img_folder = 'obsidian/files',
-			img_name_func = function()
-				return string.format('%s-%s', os.time())
-			end,
-
-			-- A function that determines the text to insert in the note when pasting an image.
-			-- It takes two arguments, the `obsidian.Client` and an `obsidian.Path` to the image file.
-			-- This is the default implementation.
-			---@param client obsidian.Client
-			---@param path obsidian.Path the absolute path to the image file
-			---@return string
-			img_text_func = function(client, path)
-				path = client:vault_relative_path(path) or path
-				return string.format('![%s](%s)', path.name, path)
-			end,
-		},
 	},
 	-- stylua: ignore start
 	keys = {
 		{ '<leader>ot', '<cmd>ObsidianToday<CR>', desc = 'Open obsidian daily today' },
 		{ '<leader>oT', '<cmd>ObsidianTemplate<CR>', desc = 'Paste obsidian template current buffer' },
-		{ '<leader>on', '<cmd>ObsidianNew<CR>', desc = 'New obsidian note' },
 	},
 }
