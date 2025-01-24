3e33a658657aac6c2d4741ce7ef7df6623a6f294
Bryant Paz <pazbryant@proton.me>
2025-01-24 12:44:58 -0600

    chore(snacks): update snacks configuration, disable zen plugin and remove unused code.

--- lua/bryant/plugins/snacks.lua
+++ lua/bryant/plugins/snacks.lua
@@ -3,50 +3,13 @@ return {
 	lazy = false,
 	priority = 1000,
 	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-		scratch = { enabled = false },
-		profiler = { enabled = false },
-		debug = { enabled = false },
-		git = { enabled = false },
-		gitbrowse = { enabled = false },
-		bufdelete = { enabled = false },
-		terminal = { enabled = false },
-		toggle = { enabled = false },
-		util = { enabled = false },
-		win = { enabled = false },
-		scroll = { enabled = false },
-		statuscolumn = { enabled = false },
-		dim = { enabled = false },
-		scope = { enabled = false },
-		notifier = { enabled = false },
-		notify = { enabled = false },
-		rename = { enabled = false },
-		animate = { enabled = false },
-		-- Enabled
 		indent = { enabled = true },
 		bigfile = { enabled = true },
 		lazygit = { enable = true },
 		quickfile = { enabled = true },
-		zen = {
-			enabled = true,
-			toggles = { dim = false },
-			on_open = function() end,
-			on_close = function() end,
-		},
-		styles = {
-			zen = {
-				width = 80,
-				backdrop = {
-					transparent = false,
-				},
-			},
-		},
 	},
 	keys = {
 		-- stylua: ignore start
-		{ 'zm', function() Snacks.zen() end, desc = 'Toggle Zen Mode' },
 		{ '<leader>gg', function() Snacks.lazygit() end, desc = 'Lazygit' },
 	},
 }
