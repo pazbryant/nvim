235aeb66d8b03b64d63d122feae8dca74013f433
Bryant Paz <pazbryant@proton.me>
2025-01-24 12:39:03 -0600

    chore(nvimcmd): remove unused autocmds

--- lua/bryant/autocmd.lua
+++ lua/bryant/autocmd.lua
@@ -229,19 +229,3 @@ autocmd({ 'FileType' }, {
 		vim.opt_local.syntax = 'editorconfig'
 	end,
 })
-
--- Automatically update changed file in Vim
--- Triger `autoread` when files changes on disk
--- https://unix.stackexchange.com/questions/149209/refresh-changed-content-of-file-opened-in-vim/383044#383044
--- https://vi.stackexchange.com/questions/13692/prevent-focusgained-autocmd-running-in-command-line-editing-mode
-autocmd({ 'FocusGained', 'BufEnter', 'CursorHold', 'CursorHoldI' }, {
-	group = bryant_group,
-	command = [[silent! if mode() != 'c' && !bufexists("[Command Line]") | checktime | endif]],
-})
-
--- Notification after file change
--- https://vi.stackexchange.com/questions/13091/autocmd-event-for-autoread
-autocmd('FileChangedShellPost', {
-	group = bryant_group,
-	command = [[echohl WarningMsg | echo "File changed on disk. Buffer reloaded." | echohl None]],
-})
