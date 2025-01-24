ca4a344d0c9f07ed6f54b22beb4d2a3fe71cc896
Bryant Paz <pazbryant@proton.me>
2025-01-23 22:31:05 -0600

    chore(flash): update neovim plugin config

--- lua/bryant/plugins/flash.lua
+++ lua/bryant/plugins/flash.lua
@@ -1,12 +1,34 @@
 return {
 	'folke/flash.nvim',
 	event = 'VeryLazy',
-	---@type Flash.Config
 	opts = {},
-	keys = {
-		-- stylua: ignore start
-		{ '<cr>', mode = { 'n', 'x', 'o' }, function() require('flash').jump() end, desc = 'Flash' },
-		{ 'ga', mode = { 'n', 'x', 'o' }, function() require('flash').treesitter() end, desc = 'Flash Treesitter' },
-		{ 'r', mode = 'o', function() require('flash').remote() end, desc = 'Flash Remote' },
-	},
+	keys = function()
+		local flash = require('flash')
+		return {
+			{
+				'<cr>',
+				mode = { 'n', 'x', 'o' },
+				function()
+					flash.jump()
+				end,
+				desc = 'Flash',
+			},
+			{
+				'ga',
+				mode = { 'n', 'x', 'o' },
+				function()
+					flash.treesitter()
+				end,
+				desc = 'Flash Treesitter',
+			},
+			{
+				'r',
+				mode = 'o',
+				function()
+					flash.remote()
+				end,
+				desc = 'Flash Remote',
+			},
+		}
+	end,
 }
