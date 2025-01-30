1f7834ddbf8f5cc71425b5cec90519bc6873e398
Bryant Paz <pazbryant@proton.me>
2025-01-29 22:34:03 -0600

    chore(obsidian): add keybinds to obsidian plugin and not to markdown files.

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -4,17 +4,3 @@ o.spc = ''
 
 vim.opt_local.wrap = true
 vim.opt_local.textwidth = 80
-
-vim.keymap.set(
-	'n',
-	'<c-t>',
-	'<cmd>ObsidianSearch<cr>',
-	{ desc = 'ObsidianSearch' }
-)
-
-vim.keymap.set(
-	'n',
-	'<m-c>',
-	'<cmd>ObsidianQuickSwitch<cr>',
-	{ desc = 'Obsidian Quick Switch' }
-)
