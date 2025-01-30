3d8c8230d4274774b24c0a7f94a6616bb437e48f
Bryant Paz <pazbryant@proton.me>
2025-01-30 15:17:51 -0600

    chore(bfq): auto_preview true by default

--- lua/bryant/plugins/nvim-bfq.lua
+++ lua/bryant/plugins/nvim-bfq.lua
@@ -3,7 +3,7 @@ return {
 	ft = 'qf',
 	opts = {
 		preview = {
-			auto_preview = false,
+			auto_preview = true,
 			winblend = 0, -- Set this to match your default transparency (0-100)
 			win_height = 30,
 			win_vheight = 30,
