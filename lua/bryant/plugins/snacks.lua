0fe2e025d8a63ca285540eaf78dc3df3f104e1a6
Bryant Paz <pazbryant@proton.me>
2025-01-29 22:35:06 -0600

    chore(snacks): update default values, activate some integrated plugins

--- lua/bryant/plugins/snacks.lua
+++ lua/bryant/plugins/snacks.lua
@@ -3,13 +3,38 @@ return {
 	lazy = false,
 	priority = 1000,
 	opts = {
+		scope = { enabled = true },
+		input = { enabled = true },
+		debug = { enabled = true },
 		indent = { enabled = true },
-		bigfile = { enabled = true },
 		lazygit = { enable = true },
+		scroll = { enabled = true },
+		bigfile = { enabled = true },
 		quickfile = { enabled = true },
+		notifier = {
+			enabled = true,
+			timeout = 1000,
+			style = 'fancy', -- compact/fancy/minimal
+			width = { min = 30, max = 0.3 },
+			height = { min = 1, max = 0.6 },
+			margin = { top = 1, right = 1, bottom = 1 },
+			padding = true,
+		},
 	},
 	keys = {
-		-- stylua: ignore start
-		{ '<leader>gg', function() Snacks.lazygit() end, desc = 'Lazygit' },
+		{
+			'<leader>n',
+			function()
+				Snacks.notifier.show_history()
+			end,
+			desc = 'Show notification history with snacks',
+		},
+		{
+			'<leader>gg',
+			function()
+				Snacks.lazygit()
+			end,
+			desc = 'Lazygit',
+		},
 	},
 }
