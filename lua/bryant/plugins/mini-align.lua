de55151337a2e5a1bdff02c1a8d2c27486cc7ffd
Bryant Paz <pazbryant@proton.me>
2025-01-30 00:52:30 -0600

    chore(mini-align): new plugin

---
+++ lua/bryant/plugins/mini-align.lua
@@ -0,0 +1,9 @@
+return {
+	'echasnovski/mini.align',
+	version = '*',
+	keys = {
+		{ 'ga', desc = 'Mini.align Align text' },
+		{ 'gA', desc = 'Mini.align Align text with preview' },
+	},
+	opts = {},
+}
