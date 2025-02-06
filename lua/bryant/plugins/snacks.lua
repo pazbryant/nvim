caf509335ae5ed41a4d01efc5806fc540ed79cb0
Bryant Paz <pazbryant7@outlook.com>
2025-02-05 19:14:37 -0600

    feat(snacks): use snacks zen mode

--- lua/bryant/plugins/snacks.lua
+++ lua/bryant/plugins/snacks.lua
@@ -10,6 +10,11 @@ return {
 		scroll = { enabled = true },
 		bigfile = { enabled = true },
 		quickfile = { enabled = true },
+		zen = {
+			enabled = true,
+			toggles = { dim = false },
+			win = { width = 85, backdrop = { transparent = false, blend = 99 } },
+		},
 		notifier = {
 			enabled = true,
 			timeout = 1000,
@@ -26,14 +31,21 @@ return {
 			function()
 				Snacks.notifier.show_history()
 			end,
-			desc = 'Show notification history with snacks',
+			desc = 'Snacks notifier',
 		},
 		{
 			'<leader>gg',
 			function()
 				Snacks.lazygit()
 			end,
-			desc = 'Lazygit',
+			desc = 'Snacks lazygit',
+		},
+		{
+			'zm',
+			function()
+				Snacks.zen()
+			end,
+			desc = 'Snacks zen',
 		},
 	},
 }
