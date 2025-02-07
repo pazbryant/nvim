0f759268768bc0875b5ea55eb8a5484ea0ed8917
Bryant Paz <pazbryant7@outlook.com>
2025-02-07 00:51:32 -0600

    chore: hide icons in bufferline

--- lua/bryant/plugins/bufferline.lua
+++ lua/bryant/plugins/bufferline.lua
@@ -11,6 +11,9 @@ return {
 					local current_buffer = vim.api.nvim_get_current_buf()
 					return buf_number == current_buffer
 				end,
+				color_icons = false, -- whether or not to add the filetype icon highlights
+				show_buffer_icons = false, -- disable filetype icons for buffers
+				show_buffer_close_icons = false,
 			},
 		})
 	end,
