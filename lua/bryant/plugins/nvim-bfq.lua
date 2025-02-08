965831e8b926c99c755be2a76e582b20c2b7bd99
Bryant Paz <pazbryant7@outlook.com>
2025-02-07 23:49:12 -0600

    feat: remove border, full screen preview

--- lua/bryant/plugins/nvim-bfq.lua
+++ lua/bryant/plugins/nvim-bfq.lua
@@ -3,10 +3,11 @@ return {
 	ft = 'qf',
 	opts = {
 		preview = {
+			winblend = 0,
+			win_height = 100,
+			win_vheight = 100,
 			auto_preview = true,
-			winblend = 0, -- Set this to match your default transparency (0-100)
-			win_height = 30,
-			win_vheight = 30,
+			border = { '', '', '', '', '', '', '', '' },
 		},
 		func_map = {
 			ptoggleauto = 'P',
