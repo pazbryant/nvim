72f6bb7acd3320d52d72eb78f12d5b9ef73950c9
Bryant Paz <pazbryant7@outlook.com>
2025-02-08 23:59:40 -0600

    feat: remove mini.align

--- lua/bryant/plugins/mini-align.lua
+++
@@ -1,9 +0,0 @@
-return {
-	'echasnovski/mini.align',
-	version = '*',
-	keys = {
-		{ 'ga', desc = 'Mini.align Align text' },
-		{ 'gA', desc = 'Mini.align Align text with preview' },
-	},
-	opts = {},
-}
