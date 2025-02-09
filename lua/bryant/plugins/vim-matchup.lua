cbac07e6a7880c29fd9c44eb22365928c1e81559
Bryant Paz <pazbryant7@outlook.com>
2025-02-08 22:08:08 -0600

    feat: disable matchparen highlight

--- lua/bryant/plugins/vim-matchup.lua
+++ lua/bryant/plugins/vim-matchup.lua
@@ -2,6 +2,7 @@ return {
 	'andymass/vim-matchup',
 	event = 'VeryLazy',
 	config = function()
+		vim.g.matchup_matchparen_enabled = 0
 		vim.g.matchup_matchparen_offscreen = { method = 'popup' }
 	end,
 }
