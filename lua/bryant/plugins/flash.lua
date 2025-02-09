31f440702fe2be327a96c7c39d2e1d544cefcd82
Bryant Paz <pazbryant7@outlook.com>
2025-02-08 23:59:34 -0600

    feat: new flash keybind. Toggle search

--- lua/bryant/plugins/flash.lua
+++ lua/bryant/plugins/flash.lua
@@ -29,6 +29,14 @@ return {
 				end,
 				desc = 'Flash Remote',
 			},
+			{
+				'<c-s>',
+				mode = { 'c' },
+				function()
+					require('flash').toggle()
+				end,
+				desc = 'Toggle Flash Search',
+			},
 		}
 	end,
 }
