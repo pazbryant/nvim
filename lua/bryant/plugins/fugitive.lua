c857ce969ceec17037752a2390604867a7a5c166
Bryant Paz <pazbryant7@outlook.com>
2025-02-13 21:29:33 -0600

    feat: git fugitive new changes

--- lua/bryant/plugins/fugitive.lua
+++ lua/bryant/plugins/fugitive.lua
@@ -2,7 +2,10 @@ return {
 	'tpope/vim-fugitive',
 	event = 'VeryLazy',
 	keys = {
-		-- stylua: ignore start
-		{ '<leader>gs', vim.cmd.Git, desc = 'Fugitive init' },
+    -- stylua: ignore start
+    { '<leader>gs', vim.cmd.Git, desc = 'Fugitive init' },
+    { '<leader>gd', '<cmd>Gvdiffsplit!<CR>', desc = 'Fugitive diff split' },
+    {'<leader>gu', '<cmd>diffget //2<CR>',  desc = 'Fugitive ours changes' },
+    { '<leader>gh', '<cmd>diffget //3<CR>', desc = 'Fugitive theirs changes' },
 	},
 }
