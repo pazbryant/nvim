b0e395b624356e2cfb396a6046c1015b8d163c85
Bryant Paz <pazbryant7@outlook.com>
2025-02-13 18:14:08 -0600

    feat: remove unused binds

--- lua/bryant/plugins/fzflua.lua
+++ lua/bryant/plugins/fzflua.lua
@@ -1,6 +1,5 @@
 return {
 	'ibhagwan/fzf-lua',
-	dependencies = 'nvim-tree/nvim-web-devicons',
 	cmd = 'FzfLua',
 	opts = {
 		profile = {
@@ -19,16 +18,13 @@ return {
 	},
 	-- stylua: ignore start
 	keys = {
-		{ '<m-b>', function() require('fzf-lua').buffers() end, desc = 'FzfLua Live grep' },
 		{ '<leader>ht', function() require('fzf-lua').help_tags() end, desc = 'FzfLua Help tags' },
 		{ '<leader>k', function() require('fzf-lua').keymaps() end, desc = 'FzfLua show keymaps' },
 		{ '<c-t>', function() require('fzf-lua').live_grep_native() end, desc = 'FzfLua Live grep' },
 		{ 'z=', function() require('fzf-lua').spell_suggest() end, desc = 'FzfLua spell suggestions' },
-		{ '<m-d>', function() require('fzf-lua').lsp_document_symbols() end, desc = 'FzfLua document symbols' },
 		{ '<leader>th', function() require('fzf-lua').colorschemes() end, desc = 'FzfLua Switch colorschemes' },
+		{ '<leader>ds', function() require('fzf-lua').lsp_document_symbols() end, desc = 'FzfLua document symbols' },
 		{ '<leader>ws', function() require('fzf-lua').lsp_workspace_symbols() end, desc = 'FzfLua workspace symbols' },
 		{ '<c-p>', function() require('fzf-lua').files({ fd_opts = "--exclude .obsidian" }) end, desc = 'FzfLua Files' },
-		{ '<m-t>', function() require('fzf-lua').live_grep_native({ cwd = vim.fn.expand('%:h') }) end, desc = 'FzfLua Live grep' },
-		{ '<m-c>', function() require('fzf-lua').files({ fd_opts = "--exclude .obsidian", cwd = vim.fn.expand('%:h') }) end, desc = 'FzfLua Files' },
 	},
 }
