efcb44565ea705d0e8fa5667d655c51042677f07
Bryant Paz <pazbryant7@outlook.com>
2025-02-13 21:34:01 -0600

    feat: update fugitive default keybinds

--- after/ftplugin/fugitive.lua
+++ after/ftplugin/fugitive.lua
@@ -8,6 +8,6 @@ local bufnr = vim.api.nvim_get_current_buf()
 local opts = { buffer = bufnr, remap = false }
 
     -- stylua: ignore start
+map('n', 'gp', function() vim.cmd.Git('push') end, opts)
 map('n', '<leader>p', ':Git push -u origin ', opts)
-map('n', '<c-p>', function() vim.cmd.Git('push') end, opts)
 map('n', '<leader>P', function() vim.cmd.Git({ 'pull', '--rebase' }) end, opts)
