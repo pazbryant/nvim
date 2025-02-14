b469c23a81e96a9f610985bf095074d72e0216a8
Bryant Paz <pazbryant7@outlook.com>
2025-02-13 18:51:10 -0600

    feat: disable fg and bg in todocomments

--- lua/bryant/plugins/todocomments.lua
+++ lua/bryant/plugins/todocomments.lua
@@ -3,7 +3,12 @@ return {
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	event = 'VimEnter',
 	cmd = { 'TodoQuickFix' },
-	opts = {},
+	opts = {
+		gui_style = {
+			fg = 'NONE', -- The gui style to use for the fg highlight group.
+			bg = 'NONE', -- The gui style to use for the bg highlight group.
+		},
+	},
 	keys = {
 		-- stylua: ignore start
 		{ '<leader>tq', '<cmd> TodoQuickFix <CR>', desc = 'Todo comments quick fix' },
