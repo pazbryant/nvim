2dbdc9c4b4b97038d28d5044cdcbf8ee01b6d0f0
Bryant Paz <pazbryant7@outlook.com>
2025-02-16 04:28:57 -0600

    feat: adding zenbone colorscheme

--- lua/bryant/plugins/colorschemes.lua
+++ lua/bryant/plugins/colorschemes.lua
@@ -46,4 +46,30 @@ return {
 			vim.cmd.colorscheme('vscode')
 		end,
 	},
+
+	{
+		'mcchrish/zenbones.nvim',
+		dependencies = 'rktjmp/lush.nvim',
+		config = function()
+			vim.cmd.colorscheme('zenbones')
+			vim.cmd([[
+      augroup MyColorSchemeTweaks
+        autocmd!
+        autocmd ColorScheme zenbones lua MyZenbonesTweaks()
+      augroup END
+    ]])
+
+			function NonItalicZenbone()
+				if vim.o.background == 'light' then
+					vim.api.nvim_set_hl(0, 'Constant', { fg = '#556570' })
+					vim.api.nvim_set_hl(0, 'Number', { fg = '#2c363c' })
+				else
+					vim.api.nvim_set_hl(0, 'Constant', { fg = '#868C91' })
+					vim.api.nvim_set_hl(0, 'Number', { fg = '#B4BDC3' })
+				end
+			end
+
+			NonItalicZenbone()
+		end,
+	},
 }
