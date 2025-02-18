4bbbccb389194df9339d7edca12559171d2506f6
Bryant Paz <pazbryant7@outlook.com>
2025-02-18 03:43:37 -0600

    feat: update default colorschemes

--- lua/bryant/plugins/colorschemes.lua
+++ lua/bryant/plugins/colorschemes.lua
@@ -1,6 +1,18 @@
 return {
-	'Mofiqul/vscode.nvim',
-	config = function()
-		vim.cmd.colorscheme('vscode')
-	end,
+	{
+		'Mofiqul/vscode.nvim',
+		config = function()
+			vim.cmd.colorscheme('vscode')
+		end,
+	},
+
+	{
+		'zenbones-theme/zenbones.nvim',
+		dependencies = 'rktjmp/lush.nvim',
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme zenbones]])
+		end,
+	},
 }
