929ebb7b3df993962e2ccaa91531a705c1870980
Bryant Paz <pazbryant7@outlook.com>
2025-02-18 04:07:17 -0600

    feat: use default neovim colorscheme

--- lua/bryant/plugins/colorschemes.lua
+++ lua/bryant/plugins/colorschemes.lua
@@ -9,7 +9,6 @@ return {
 	{
 		'zenbones-theme/zenbones.nvim',
 		dependencies = 'rktjmp/lush.nvim',
-		lazy = false,
 		priority = 1000,
 		config = function()
 			vim.cmd([[colorscheme zenbones]])
