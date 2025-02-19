c5670865c2d09f84300906f7c3d330dddc5b2fcc
Bryant Paz <pazbryant7@outlook.com>
2025-02-18 20:41:13 -0600

    feat: new language servers config

--- lua/bryant/plugins/lsp/setup.lua
+++ lua/bryant/plugins/lsp/setup.lua
@@ -59,6 +59,41 @@ local capabilities = vim.tbl_deep_extend(
 )
 
 local servers = {
+	jsonls = {
+		settings = {
+			json = {
+				schemas = require('schemastore').json.schemas({
+					ignore = {
+						-- '.eslintrc',
+						-- 'package.json',
+					},
+				}),
+				validate = { enable = true },
+			},
+		},
+	},
+
+	yamlls = {
+		settings = {
+			yaml = {
+				schemaStore = {
+					-- You must disable built-in schemaStore support if you want to use
+					-- this plugin and its advanced options like `ignore`.
+					enable = false,
+					-- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+					url = '',
+				},
+				schemas = require('schemastore').json.schemas({
+					ignore = {
+						-- '.eslintrc',
+						-- 'package.json',
+					},
+				}),
+				validate = { enable = true },
+			},
+		},
+	},
+
 	gopls = {
 		cmd = { 'gopls' },
 		settings = {
