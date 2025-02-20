da1688ad6eba6c5bf4f55e76219281930b6a741b
Bryant Paz <pazbryant7@outlook.com>
2025-02-19 22:51:05 -0600

    feat: updating player-one plugin configuration

--- lua/bryant/plugins/player-one.lua
+++ lua/bryant/plugins/player-one.lua
@@ -1,10 +1,7 @@
 return {
 	'jackplus-xyz/player-one.nvim',
 	lazy = false,
-	config = function()
-		local Config = require('player-one.config')
-		Config.setup({
-			is_enabled = true,
-		})
-	end,
+	opts = {
+		is_enabled = true,
+	},
 }
