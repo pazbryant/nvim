2f84dfb9b30bbf16500d393315cae91396c6b815
Bryant Paz <pazbryant7@outlook.com>
2025-02-23 03:59:03 -0600

    feat(fzf-lua): using trouble for fzf-lua

--- lua/bryant/plugins/fzflua.lua
+++ lua/bryant/plugins/fzflua.lua
@@ -1,21 +1,28 @@
 return {
 	'ibhagwan/fzf-lua',
 	cmd = 'FzfLua',
-	opts = {
-		profile = {
-			enabled = true,
-			default = 'max-perf', -- Fixed typo from 'max-pref'
-		},
-		keymap = {
-			fzf = {
-				['ctrl-q'] = 'select-all+accept',
+	opts = function()
+		local actions = require('fzf-lua').actions
+		local trouble_actions = require('trouble.sources.fzf').actions
+		return {
+			profile = {
+				enabled = true,
+				default = 'max-perf', -- Fixed typo from 'max-pref'
 			},
-		},
-		winopts = {
-			height = 0.70,
-			width = 0.70,
-		},
-	},
+			winopts = {
+				height = 0.70,
+				width = 0.70,
+			},
+			actions = {
+				files = {
+					['ctrl-s'] = actions.file_split,
+					['ctrl-v'] = actions.file_vsplit,
+					['enter'] = actions.file_edit_or_qf,
+					['ctrl-q'] = trouble_actions.open_all,
+				},
+			},
+		}
+	end,
 	-- stylua: ignore start
 	keys = {
 		{ '<leader>ht', function() require('fzf-lua').help_tags() end, desc = 'FzfLua Help tags' },
@@ -23,7 +30,7 @@ return {
 		{ '<c-t>', function() require('fzf-lua').live_grep_native() end, desc = 'FzfLua Live grep' },
 		{ 'z=', function() require('fzf-lua').spell_suggest() end, desc = 'FzfLua spell suggestions' },
 		{ '<leader>th', function() require('fzf-lua').colorschemes() end, desc = 'FzfLua Switch colorschemes' },
-		{ '<leader>ds', function() require('fzf-lua').lsp_document_symbols() end, desc = 'FzfLua document symbols' },
+		{ '<leader>ps', function() require('fzf-lua').lsp_document_symbols() end, desc = 'FzfLua document symbols' },
 		{ '<leader>ws', function() require('fzf-lua').lsp_workspace_symbols() end, desc = 'FzfLua workspace symbols' },
 		{ '<c-p>', function() require('fzf-lua').files({ fd_opts = "--exclude .obsidian" }) end, desc = 'FzfLua Files' },
 	},
