30188760f1531fe756e9ad4880cdf3000fe0b22d
Bryant Paz <pazbryant7@outlook.com>
2025-03-02 13:38:09 -0600

    feat(fugitive): adding notifications to git theirs and ours keybinds

--- lua/bryant/plugins/fugitive.lua
+++ lua/bryant/plugins/fugitive.lua
@@ -2,10 +2,30 @@ return {
 	'tpope/vim-fugitive',
 	event = 'VeryLazy',
 	keys = {
-    -- stylua: ignore start
-    { '<leader>gs', vim.cmd.Git, desc = 'Fugitive init' },
-    { '<leader>gd', '<cmd>Gvdiffsplit!<CR>', desc = 'Fugitive diff split' },
-    {'<leader>go', '<cmd>diffget //2<CR>',  desc = 'Fugitive ours changes' },
-    { '<leader>gt', '<cmd>diffget //3<CR>', desc = 'Fugitive theirs changes' },
+		{ '<leader>gs', vim.cmd.Git, desc = 'Fugitive init' },
+		{
+			'<leader>gd',
+			function()
+				vim.cmd([[Gvdiffsplit!]])
+				vim.notify('Resolving git conflicts', vim.log.levels.INFO)
+			end,
+			desc = 'Fugitive diff split',
+		},
+		{
+			'<leader>go',
+			function()
+				vim.cmd([[diffget //2]])
+				vim.notify('Git ours selected', vim.log.levels.INFO)
+			end,
+			desc = 'Fugitive ours changes',
+		},
+		{
+			'<leader>gt',
+			function()
+				vim.cmd([[diffget //3]])
+				vim.notify('Git theirs selected', vim.log.levels.INFO)
+			end,
+			desc = 'Fugitive ours changes',
+		},
 	},
 }
