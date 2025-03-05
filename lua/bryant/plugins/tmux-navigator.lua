3790118500f8c59167608455a9fa8c09b0d0e28d
Bryant Paz <pazbryant7@outlook.com>
2025-03-04 18:38:31 -0600

    feat(neovim): last neovim update config. Now time to work

--- lua/bryant/plugins/tmux-navigator.lua
+++ lua/bryant/plugins/tmux-navigator.lua
@@ -1,16 +1,15 @@
 return {
 	'christoomey/vim-tmux-navigator',
-	cmd = {
-		'TmuxNavigateLeft',
-		'TmuxNavigateDown',
-		'TmuxNavigateUp',
-		'TmuxNavigateRight',
-		'TmuxNavigatePrevious',
-	},
-	keys = {
-		{ '<C-h>', '<cmd><C-U>TmuxNavigateLeft<cr>', desc = 'Tmux left' },
-		{ '<C-j>', '<cmd><C-U>TmuxNavigateDown<cr>', desc = 'Tmux down' },
-		{ '<C-k>', '<cmd><C-U>TmuxNavigateUp<cr>', desc = 'Tmux up' },
-		{ '<C-l>', '<cmd><C-U>TmuxNavigateRight<cr>', desc = 'Tmux right' },
-	},
+	lazy = false,
+	init = function()
+		vim.g.tmux_navigator_no_mappings = 1
+		vim.g.tmux_navigator_disable_when_zoomed = 1
+
+		local keymap = vim.keymap.set
+		keymap('n', '<A-k>', ':<C-U>TmuxNavigateUp<CR>', { silent = true })
+		keymap('n', '<A-j>', ':<C-U>TmuxNavigateDown<CR>', { silent = true })
+		keymap('n', '<A-h>', ':<C-U>TmuxNavigateLeft<CR>', { silent = true })
+		keymap('n', '<A-l>', ':<C-U>TmuxNavigateRight<CR>', { silent = true })
+		keymap('n', '<A-p>', ':<C-U>TmuxNavigatePrevious<CR>', { silent = true })
+	end,
 }
