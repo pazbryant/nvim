dd639be364fd4b74849760996242a5c636671215
Bryant Paz <pazbryant7@outlook.com>
2025-03-08 02:27:37 -0600

    feat(zenbones): remove colorscheme

--- lua/bryant/plugins/colorschemes.lua
+++ lua/bryant/plugins/colorschemes.lua
@@ -13,32 +13,4 @@ return {
 			vim.cmd.colorscheme('moonfly')
 		end,
 	},
-
-	{
-		'zenbones-theme/zenbones.nvim',
-		lazy = false,
-		priority = 100,
-		dependencies = 'rktjmp/lush.nvim',
-		config = function()
-			vim.cmd.colorscheme('zenbones')
-			vim.cmd([[
-      augroup MyColorSchemeTweaks
-        autocmd!
-        autocmd ColorScheme zenbones lua MyZenbonesTweaks()
-      augroup END
-    ]])
-
-			function MyZenbonesTweaks()
-				if vim.o.background == 'light' then
-					vim.api.nvim_set_hl(0, 'Constant', { fg = '#556570' })
-					vim.api.nvim_set_hl(0, 'Number', { fg = '#2c363c' })
-				else
-					vim.api.nvim_set_hl(0, 'Constant', { fg = '#868C91' })
-					vim.api.nvim_set_hl(0, 'Number', { fg = '#B4BDC3' })
-				end
-			end
-
-			MyZenbonesTweaks()
-		end,
-	},
 }
