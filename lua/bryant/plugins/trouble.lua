df8cd023d8bd3246c0569da5a01140944493b7cf
Bryant Paz <pazbryant7@outlook.com>
2025-03-13 13:25:32 -0600

    feat(trouble,fzf-lua,aerial): udpate default settings

--- lua/bryant/plugins/trouble.lua
+++ lua/bryant/plugins/trouble.lua
@@ -14,13 +14,11 @@ return {
 		-- Window options for the preview window. Can be a split, floating window,
 		-- or `main` to show the preview in the main editor window.
 		keys = {
-			['?'] = 'help',
+			['g?'] = 'help',
 		},
 	},
 	-- stylua: ignore start
 	keys = {
-    { '<leader>xg', '<cmd>Trouble fzf<cr>', desc = 'Trouble fzf grep' },
-    { '<leader>xf', '<cmd>Trouble fzf_files<cr>', desc = 'Trouble fzf files' },
     { '<leader>q', '<cmd>Trouble qflist toggle<cr>', desc = 'Trouble quickfix' },
     { '<leader>xX', '<cmd>Trouble diagnostics toggle<cr>', desc = 'Diagnostics (Trouble)' },
     { '<leader>xx', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', desc = 'Buffer Diagnostics (Trouble)' },
