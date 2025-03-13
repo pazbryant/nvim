cd27ff4e669d6aa2351d3967f5d0c8fc648a8fb4
Bryant Paz <pazbryant7@outlook.com>
2025-03-13 13:25:32 -0600

    feat(trouble,fzf-lua,aerial): udpate default settings

--- lua/bryant/plugins/fzflua.lua
+++ lua/bryant/plugins/fzflua.lua
@@ -82,7 +82,7 @@ return {
     { '<c-t>', function() require('fzf-lua').live_grep_native() end, desc = 'FzfLua Live grep' },
     { 'z=', function() require('fzf-lua').spell_suggest() end, desc = 'FzfLua spell suggestions' },
     { '<leader>th', function() require('fzf-lua').colorschemes() end, desc = 'FzfLua Switch colorschemes' },
-    { '<leader>ps', function() require('fzf-lua').lsp_document_symbols() end, desc = 'FzfLua document symbols' },
+    { '<leader>ds', function() require('fzf-lua').lsp_document_symbols() end, desc = 'FzfLua document symbols' },
     { '<leader>ws', function() require('fzf-lua').lsp_workspace_symbols() end, desc = 'FzfLua workspace symbols' },
   },
 }
