7bd28b1857bfd324a9fb5f1d815e36c03af27d4e
Bryant Paz <pazbryant7@outlook.com>
2025-03-28 19:09:15 -0600

    feat(blink): disable lsp sniipets

--- lua/bryant/plugins/blink.lua
+++ lua/bryant/plugins/blink.lua
@@ -9,24 +9,17 @@ return {
 			version = 'v2.*',
 			config = function()
 				local ls = require('luasnip')
-
 				ls.config.set_config({
 					history = true,
 					updateevents = 'TextChanged,TextChangedI',
 					enable_autosnippets = true,
 				})
-
 				require('luasnip.loaders.from_vscode').lazy_load({
 					exclude = { 'sh', 'bash' },
 				})
-
 				require('luasnip.loaders.from_lua').lazy_load({
 					paths = { './snippets' },
 				})
-
-				ls.filetype_extend('javascript', { 'javascriptreact' })
-				ls.filetype_extend('typescript', { 'javascript' })
-				ls.filetype_extend('typescriptreact', { 'javascript', 'typescript' })
 			end,
 		},
 	},
@@ -34,16 +27,42 @@ return {
 		cmdline = { enabled = false },
 		completion = {
 			documentation = { auto_show = true, auto_show_delay_ms = 500 },
+			menu = {
+				draw = {
+					columns = {
+						{ 'label', 'label_description', gap = 1 },
+						{ 'kind_icon', 'kind', gap = 1 },
+					},
+				},
+			},
 		},
 		sources = {
-			default = { 'lazydev', 'lsp', 'path', 'buffer', 'snippets' },
+			default = { 'lazydev', 'snippets', 'lsp', 'path', 'buffer' },
 			providers = {
+				snippets = {
+					score_offset = 100,
+				},
 				lazydev = {
 					name = 'LazyDev',
 					module = 'lazydev.integrations.blink',
 					score_offset = 100,
 				},
 			},
+			transform_items = function(_, items)
+				local wanted = {}
+				for _, item in ipairs(items) do
+					if
+						not (
+							item.kind
+								== require('blink.cmp.types').CompletionItemKind.Snippet
+							and item.source_id == 'lsp'
+						)
+					then
+						wanted[#wanted + 1] = item
+					end
+				end
+				return wanted
+			end,
 		},
 		fuzzy = { implementation = 'prefer_rust_with_warning' },
 		snippets = { preset = 'luasnip' },
