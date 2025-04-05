f326514885fd585f90e215dec2a73f1fe73e29fe
Bryant Paz <pazbryant7@outlook.com>
2025-04-05 13:21:00 -0600

    feat(harpoon): fix setttings

--- lua/bryant/plugins/harpoon.lua
+++ lua/bryant/plugins/harpoon.lua
@@ -15,10 +15,10 @@ return {
 		return {
       { "<leader>hn", function() harpoon2_list:next() end, desc = 'harpoon next' },
       { "<leader>hp", function() harpoon2_list:prev() end, desc = 'harpoon previous' },
-      { '<leader>%', function() harpoon2_list:select(2) end, desc = 'harpoon to file 2' },
-      { '<leader>(', function() harpoon2_list:select(4) end, desc = 'harpoon to file 4' },
-      { '<leader>{', function() harpoon2_list:select(3) end, desc = 'harpoon to file 3' },
       { '<leader>[', function() harpoon2_list:select(1) end, desc = 'harpoon to file 1' },
+      { '<leader>{', function() harpoon2_list:select(2) end, desc = 'harpoon to file 2' },
+      { '<leader>(', function() harpoon2_list:select(3) end, desc = 'harpoon to file 3' },
+      { '<leader>%', function() harpoon2_list:select(4) end, desc = 'harpoon to file 4' },
       { '<c-e>', function() harpoon2.ui:toggle_quick_menu(harpoon2:list()) end, desc = 'harpoon quick menu' },
       { '<leader>a', function() harpoon2_list:add() vim.notify('harpoon file added', vim.log.levels.INFO) end, desc = 'harpoon file' },
 		}
