926a478588ec1ffb0971cb92de462fa9eb167001
Bryant Paz <pazbryant7@outlook.com>
2025-04-07 13:54:39 -0600

    feat(blink): update default keybinds

--- lua/bryant/plugins/blink.lua
+++ lua/bryant/plugins/blink.lua
@@ -86,6 +86,23 @@ return {
 		fuzzy = { implementation = 'prefer_rust_with_warning' },
 		snippets = { preset = 'luasnip' },
 		signature = { enabled = true },
+
+		keymap = {
+			['<C-e>'] = { 'hide' },
+			['<C-y>'] = { 'select_and_accept' },
+			['<Up>'] = { 'select_prev', 'fallback' },
+			['<Down>'] = { 'select_next', 'fallback' },
+			['<C-l>'] = { 'snippet_forward', 'fallback' },
+			['<Tab>'] = { 'snippet_forward', 'fallback' },
+			['<C-h>'] = { 'snippet_backward', 'fallback' },
+			['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+			['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+			['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+			['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+			['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+			['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
+			['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+		},
 	},
 	opts_extend = { 'sources.default' },
 }
