31dcb816d466195b211d9d92016e76d2b0796e16
Bryant Paz <pazbryant7@outlook.com>
2025-05-01 14:08:21 -0600

    feat(plugins): remove unused code, remove unused autcmds and usercmds

--- lua/bryant/plugins/colorschemes.lua
+++ lua/bryant/plugins/colorschemes.lua
@@ -1,21 +1,21 @@
 return {
 	{
-		'Mofiqul/vscode.nvim',
-		config = function()
-			vim.cmd.colorscheme('vscode')
-		end,
-	},
-	{
 		'bluz71/vim-moonfly-colors',
+		lazy = false,
+		priority = 1000,
 		name = 'moonfly',
 		config = function()
 			vim.cmd.colorscheme('moonfly')
 		end,
 	},
 	{
+		'Mofiqul/vscode.nvim',
+		config = function()
+			vim.cmd.colorscheme('vscode')
+		end,
+	},
+	{
 		'zenbones-theme/zenbones.nvim',
-		lazy = false,
-		priority = 1000,
 		dependencies = 'rktjmp/lush.nvim',
 		config = function()
 			vim.cmd.colorscheme('zenbones')
