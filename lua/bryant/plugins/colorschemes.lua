f9dfedce9425d0069675dcd96de471f54dab9329
Bryant Paz <pazbryant7@outlook.com>
2025-05-05 18:43:38 -0600

    feat(habamax): set new default colorscheme

--- lua/bryant/plugins/colorschemes.lua
+++ lua/bryant/plugins/colorschemes.lua
@@ -1,14 +1,5 @@
 return {
 	{
-		'bluz71/vim-moonfly-colors',
-		lazy = false,
-		priority = 1000,
-		name = 'moonfly',
-		config = function()
-			vim.cmd.colorscheme('moonfly')
-		end,
-	},
-	{
 		'Mofiqul/vscode.nvim',
 		config = function()
 			vim.cmd.colorscheme('vscode')
