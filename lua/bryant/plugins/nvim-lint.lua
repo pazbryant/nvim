3f09011aff99f01b7ef676193c3c313d392c15da
Bryant Paz <pazbryant7@outlook.com>
2025-05-12 01:37:51 -0600

    feat(nvim-lint): fix default configuration

--- lua/bryant/plugins/nvim-lint.lua
+++ lua/bryant/plugins/nvim-lint.lua
@@ -17,20 +17,9 @@ return {
 			javascriptreact = { 'eslint_d' },
 		}
 
-		local v = vim
-
-		-- this works with the newest eslint configuration, not older one
-		local eslint_d = require('lint.linters.eslint_d')
-		eslint_d.args = v.tbl_extend('force', {
-			'--config',
-			function()
-				return v.fn.getcwd() .. '/eslint.config.js'
-			end,
-		}, eslint_d.args)
-
-		local usercmd = v.api.nvim_create_user_command
+		local usercmd = vim.api.nvim_create_user_command
 		usercmd('Lint', function()
-			require('lint').try_lint()
+			lint.try_lint(nil, { ignore_errors = true })
 			vim.notify('Code has been linting', vim.log.levels.INFO)
 		end, { desc = 'Lint file with nvim-lint' })
 	end,
