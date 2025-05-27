8d57d5225b54ff46367501d28148388ed355afc2
Bryant Paz <pazbryant7@outlook.com>
2025-05-26 18:16:01 -0600

    feat(plugins): tmux navigator new plugin, update harpoon keybinds

---
+++ lua/bryant/plugins/tmux-navigator.lua
@@ -0,0 +1,17 @@
+return {
+	'christoomey/vim-tmux-navigator',
+	cmd = {
+		'TmuxNavigateLeft',
+		'TmuxNavigateDown',
+		'TmuxNavigateUp',
+		'TmuxNavigateRight',
+		'TmuxNavigatePrevious',
+	},
+	keys = {
+		{ '<C-h>', '<cmd><C-U>TmuxNavigateLeft<cr>', desc = 'Tmux left' },
+		{ '<C-j>', '<cmd><C-U>TmuxNavigateDown<cr>', desc = 'Tmux down' },
+		{ '<C-k>', '<cmd><C-U>TmuxNavigateUp<cr>', desc = 'Tmux up' },
+		{ '<C-l>', '<cmd><C-U>TmuxNavigateRight<cr>', desc = 'Tmux right' },
+		{ '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>', desc = 'Tmux previous' },
+	},
+}
