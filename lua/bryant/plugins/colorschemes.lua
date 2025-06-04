d10a325d9ec7e99d7b6374671e087f434445b07b
Bryant Paz <pazbryant7@outlook.com>
2025-06-03 23:15:23 -0600

    feat: updatig default colorschemes

--- lua/bryant/plugins/colorschemes.lua
+++ lua/bryant/plugins/colorschemes.lua
@@ -1,15 +1,53 @@
 return {
 	{
+		lazy = false,
+		priority = 1000,
 		'Mofiqul/vscode.nvim',
 		config = function()
+			require('vscode').setup({
+				transparent = false,
+				italic_comments = false,
+			})
 			vim.cmd.colorscheme('vscode')
 		end,
 	},
+
 	{
-		'zenbones-theme/zenbones.nvim',
-		dependencies = 'rktjmp/lush.nvim',
+		'rose-pine/neovim',
 		config = function()
-			vim.cmd.colorscheme('zenbones')
+			require('rose-pine').setup({
+				styles = {
+					bold = true,
+					italic = false,
+					transparency = false,
+				},
+			})
+			vim.cmd.colorscheme('rose-pine')
+		end,
+	},
+
+	{
+		'craftzdog/solarized-osaka.nvim',
+		config = function()
+			require('solarized-osaka').setup({
+				transparent = false,
+				styles = {
+					comments = { italic = false },
+					keywords = { italic = false },
+				},
+			})
+			vim.cmd.colorscheme('solarized-osaka')
+		end,
+	},
+
+	{
+		'rebelot/kanagawa.nvim',
+		config = function()
+			require('kanagawa').setup({
+				commentStyle = { italic = false },
+				keywordStyle = { italic = false },
+			})
+			vim.cmd.colorscheme('kanagawa')
 		end,
 	},
 }
