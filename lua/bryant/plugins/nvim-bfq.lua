1fe1a862c81fe09f325f4abaeadf2cff1c84f08f
Bryant Paz <pazbryant7@outlook.com>
2025-06-03 23:24:01 -0600

    feat(bqf): disable auto preview

--- lua/bryant/plugins/nvim-bfq.lua
+++ lua/bryant/plugins/nvim-bfq.lua
@@ -3,7 +3,7 @@ return {
 	lazy = false,
 	opts = {
 		preview = {
-			auto_preview = true,
+			auto_preview = false,
 			winblend = 0, -- Set this to match your default transparency (0-100)
 			win_height = 30,
 			win_vheight = 30,
