2eba7cffc947c816053bc235aa6878e0d1bdd2b6
Bryant Paz <pazbryant7@outlook.com>
2025-06-03 23:23:45 -0600

    feat(lualine): new plugin added

---
+++ lua/bryant/plugins/lualine.lua
@@ -0,0 +1,12 @@
+return {
+	lazy = false,
+	'nvim-lualine/lualine.nvim',
+	opts = {
+		options = {
+			icons_enabled = false,
+			theme = 'auto',
+			component_separators = { left = '', right = '' },
+			section_separators = { left = '', right = '' },
+		},
+	},
+}
